ae7876c78d6927cbeaba559d6ac43c22247a7cad
Diego Rosso <diegorosso1988@gmail.com>
2024-08-05 11:51:47 -0300

    primeros cambios de cultura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import  {HomeComponent} from './components/home/home.component'
 import  {AboutComponent} from './components/about/about.component'
-import { SeccionComponent } from './components/seccion/seccion.component';
+
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, SeccionComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild('hoverVideo', { static: true }) hoverVideo!: ElementRef<HTMLVideoElement>;
 
+  ngAfterViewInit() {
+    const videoElement = this.hoverVideo.nativeElement;
+
+    videoElement.addEventListener('mouseover', () => {
+      videoElement.play();
+    });
+
+    videoElement.addEventListener('mouseout', () => {
+      videoElement.pause();
+      videoElement.currentTime = 0; // Reiniciar el video al principio si se desea
+    });
+  }
 }
